4b9a72af36728e2193197aeb453bb061979bffc6
vtjvlad <vtj.vlad@gmail.com>
2026-07-07 10:14:54 +0000

    0.9.9

--- public/cushion_rubber.js
+++ public/cushion_rubber.js
@@ -47,19 +47,63 @@ function rubberCollisionLinesFrom(line) {
     let oy1 = iy1;
     let ox2 = ix2;
     let oy2 = iy2;
+    const minCurve = 0.5;
+    const maxCurve = t * 0.85;
 
     if (chamferEndAngle != null) {
         const runEnd = chamferRunAlongEdge(t, chamferEndAngle);
         ox2 = ix2 - tx * runEnd;
         oy2 = iy2 - ty * runEnd;
-        lines.push({ x1: x2, y1: y2, x2: ox2, y2: oy2 });
+        const curve = Math.min(runEnd * 0.65, maxCurve);
+        const chamferDx = ox2 - x2;
+        const chamferDy = oy2 - y2;
+        const chamferLen = Math.hypot(chamferDx, chamferDy) || 1;
+        const chamferUx = chamferDx / chamferLen;
+        const chamferUy = chamferDy / chamferLen;
+        const curveSafe = Math.min(curve, chamferLen * 0.45);
+        const hasCurve = curveSafe >= minCurve;
+
+        if (hasCurve) {
+            const chamferCutX = ox2 - chamferUx * curveSafe;
+            const chamferCutY = oy2 - chamferUy * curveSafe;
+            const edgeCutX = ox2 - tx * curveSafe;
+            const edgeCutY = oy2 - ty * curveSafe;
+
+            lines.push({ x1: x2, y1: y2, x2: chamferCutX, y2: chamferCutY });
+            lines.push({ x1: chamferCutX, y1: chamferCutY, x2: edgeCutX, y2: edgeCutY });
+            ox2 = edgeCutX;
+            oy2 = edgeCutY;
+        } else {
+            lines.push({ x1: x2, y1: y2, x2: ox2, y2: oy2 });
+        }
     }
 
     if (chamferStartAngle != null) {
         const runStart = chamferRunAlongEdge(t, chamferStartAngle);
         ox1 = ix1 + tx * runStart;
         oy1 = iy1 + ty * runStart;
-        lines.push({ x1: ox1, y1: oy1, x2: x1, y2: y1 });
+        const curve = Math.min(runStart * 0.65, maxCurve);
+        const chamferDx = x1 - ox1;
+        const chamferDy = y1 - oy1;
+        const chamferLen = Math.hypot(chamferDx, chamferDy) || 1;
+        const chamferUx = chamferDx / chamferLen;
+        const chamferUy = chamferDy / chamferLen;
+        const curveSafe = Math.min(curve, chamferLen * 0.45);
+        const hasCurve = curveSafe >= minCurve;
+
+        if (hasCurve) {
+            const edgeCutX = ox1 + tx * curveSafe;
+            const edgeCutY = oy1 + ty * curveSafe;
+            const chamferCutX = ox1 + chamferUx * curveSafe;
+            const chamferCutY = oy1 + chamferUy * curveSafe;
+
+            lines.push({ x1: edgeCutX, y1: edgeCutY, x2: chamferCutX, y2: chamferCutY });
+            lines.push({ x1: chamferCutX, y1: chamferCutY, x2: x1, y2: y1 });
+            ox1 = edgeCutX;
+            oy1 = edgeCutY;
+        } else {
+            lines.push({ x1: ox1, y1: oy1, x2: x1, y2: y1 });
+        }
     }
 
     lines.push({ x1: ox1, y1: oy1, x2: ox2, y2: oy2 });
