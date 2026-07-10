9295cfa02d66a4e4fdfc4004b2c451d1274e56d9
vtjvlad <vtj.vlad@gmail.com>
2026-07-10 02:50:25 +0000

    1.1.5. math3r commplite

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,23 +10,13 @@ export default defineConfig({
     },
 
 
-server: {
-    host: '0.0.0.0',    // принимать подключения со всех интерфейсов
-    https: true,       // использовать HTTPS
-    hostname: [ 'vtjvlad.ddns.net', ], // имя хост
-    proxy: {
-      '/': {
-        target: 'http://172.26.3.19:5173', // адрес вашего API
-        changeOrigin: true,
-        secure: false, // если ваш API использует самоподписанный сертификат
-      }
-    },
-    port: 5173,          // порт для сервера разработки
+server: { 
+    allowedHosts: ['vtjvlad.ddns.net'], // разрешенные хосты websocket
+   // https: true,       // использовать HTTPS
+    port: 3000,          // порт для сервера разработки
     strictPort: true,   // не менять порт автоматически
     hmr: {
-      protocol: 'wss',
       host: 'vtjvlad.ddns.net',
-      port: 443 
     }
   },
                build: { /* ... */ }
