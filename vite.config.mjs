5ac3e22b091c00753288b4071ecb7fa841cbb80b
vtjvlad <vtj.vlad@gmail.com>
2026-07-10 00:58:28 +0000

    rebuild continue.

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,5 +7,29 @@ export default defineConfig({
         emptyOutDir: true,
         minify: false,
         sourcemap: true
+    },
+
+
+server: {
+    host: '0.0.0.0',    // принимать подключения со всех интерфейсов
+    https: true,       // использовать HTTPS
+    hostname: [ 'vtjvlad.ddns.net', ], // имя хост
+    proxy: {
+      '/': {
+        target: 'http://172.26.3.19:5173', // адрес вашего API
+        changeOrigin: true,
+        secure: false, // если ваш API использует самоподписанный сертификат
+      }
+    },
+    port: 5173,          // порт для сервера разработки
+    strictPort: true,   // не менять порт автоматически
+    hmr: {
+      protocol: 'wss',
+      host: 'vtjvlad.ddns.net',
+      port: 443 
     }
+  },
+               build: { /* ... */ }
 });
+
+
